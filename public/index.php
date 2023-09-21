0dea520a733bf268c08bb2f77b706773c79b71e0
Ciprian-hub <cipssul@gmail.com>
2023-09-21 17:20:39 +0300

    wip

--- public/index.php
+++ public/index.php
@@ -1,15 +1,14 @@
 <?php
+
+use app\controllers\SiteController;
 use app\core\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
-$app->router->get('/', function () {
-    return "Hello World";
-});
-$app->router->get('/contact', function (){
-    return 'Contact';
-});
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
 
 $app->run();
