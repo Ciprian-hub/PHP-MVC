b4b690c49865629902e0a9a30a02bba999aabfe0
Valea Ciprian <valeaciprian@Valeas-MacBook-Pro-2.local>
2023-09-26 21:11:29 +0300

    add authController

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use app\controllers\SiteController;
 use app\core\Application;
+use app\controllers\AuthController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -10,5 +11,10 @@ $app = new Application(dirname(__DIR__));
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
